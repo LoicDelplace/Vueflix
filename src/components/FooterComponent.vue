b79479db410131bd62fc6b8133057cbd357b1c15
Loic Delplace <loic.delplace59@gmail.com>
2023-08-21 12:25:02 +0200

    📄 Added Footer Component file

---
+++ src/components/FooterComponent.vue
@@ -0,0 +1,10 @@
+<template>
+    <footer>
+        <p>Copyright {{ yearDate }} - VueFlix - Votre répertoire de <b>{{ moviesNb }}</b> films.</p>
+    </footer>
+</template>
+
+<script setup>
+const yearDate = new Date().getFullYear();
+const moviesNb = 24592;
+</script>
