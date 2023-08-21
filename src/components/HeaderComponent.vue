c6b1258c5c18069cea9991f0aae426dfd0adc426
Loic Delplace <loic.delplace59@gmail.com>
2023-08-21 11:56:50 +0200

    📄 Added Header Component

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,20 @@
+<template>
+    <header>
+      <div class="wrapper">
+        <nav>
+          <RouterLink to="/">Home</RouterLink>
+          <RouterLink to="/about">A propos</RouterLink>
+        </nav>
+        <div class="account">
+
+        </div>
+      </div>
+    </header>
+  </template>
+  
+  <script setup>
+  import { RouterLink } from 'vue-router'
+  
+  
+  </script>
+  
