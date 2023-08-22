479a35ac794a9585d9ccbc56466c2a6a1da2b871
Loic Delplace <loic.delplace59@gmail.com>
2023-08-22 09:58:11 +0200

    ⚙️ Added toggle bio button with emits & props & ✨

--- src/components/CardComponent.vue
+++ src/components/CardComponent.vue
@@ -1,28 +1,39 @@
 <script setup>
-import { defineProps } from 'vue'
+import { defineProps, defineEmits } from 'vue'
+import Button from '@/components/ButtonComponent.vue'
 
-const props = defineProps({
-  user: {
-    type: Object,
-    required: true
-  }
-})
+const props = defineProps(['user', 'open'])
+const emit = defineEmits(['opened'])
 
-const user = props.user
+const toggleBio = () => {
+  emit('opened', props.user)
+}
 </script>
 
 <template>
   <div class="card-container">
     <div class="card">
       <div class="card-info">
-        <img :src="user.avatar" alt="user picture" />
-        <h3>
-          <img :src="'https://flagsapi.com/' + user.country + '/flat/32.png'"> {{ user.nickname }},
-          {{ user.age }} ans
-        </h3>
+        <img :src="user.avatar" alt="user picture" class="avatarImg" />
+        <div class="card-text-container">
+          <h3>
+            <img
+              :src="'https://flagsapi.com/' + user.country + '/flat/32.png'"
+              :alt="'flag' + user.country"
+            />
+            <div class="card-info-text">
+              <div class="nickname-age-container">
+                <span class="nickname">{{ user.nickname }},</span>
+                <span class="age">{{ user.age }} </span>
+              </div>
+            </div>
+          </h3>
+          <h4 class="job">{{ user.job }}</h4>
+        </div>
       </div>
       <div class="card-bio">
-        <p>{{ user.bio }}</p>
+        <Button @click="toggleBio" v-if="!open">Voir la biographie</Button>
+        <p v-if="open">{{ user.bio }}</p>
       </div>
     </div>
   </div>
@@ -39,23 +50,59 @@ const user = props.user
   flex-wrap: wrap;
   gap: 1rem;
   .card {
-    width: 30rem;
+    width: 20rem;
     height: 20rem;
     background-color: #2c3e50;
     color: white;
     display: flex;
-    align-items: center;
     flex-direction: column;
     gap: 1rem;
     border-radius: 1.2rem;
     padding: 1rem;
-    margin-bottom: 3rem;
+
     .card-info {
-        h3 {
-            display: flex;
-            align-items: center;
-            gap: 1rem;
-        }
+      display: flex;
+      align-items: center;
+      .avatarImg {
+        float: left;
+        width: 7rem;
+        height: 7rem;
+        border-radius: 50%;
+        object-fit: cover;
+        margin-right: 1rem;
+      }
+      h3 {
+        display: flex;
+        align-items: center;
+      }
+      .card-info-text {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        gap: 0.5rem;
+      }
+      .job {
+        font-weight: bold;
+        font-size: 1.2rem;
+        color: #e67e22;
+      }
+    }
+
+    .card-bio {
+      width: 100%;
+      height: 100%;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      p {
+        font-size: 1.2rem;
+        max-width: max-content;
+        width: 100%;
+        height: 100%;
+        overflow: auto;
+        text-align: justify;
+      }
     }
   }
 }
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,61 +1,72 @@
 <script setup>
-import AboutCard from '@/components/CardComponent.vue';
+import { ref } from 'vue'
+import AboutCard from '@/components/CardComponent.vue'
 
 const team = [
-    {
-        id: 1,
-        nickname: 'Gevail',
-        avatar: 'https://i.pravatar.cc/150?u=gevail',
-        bio: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis corrupti ipsum odio, sed aut ipsam provident blanditiis, molestias veritatis impedit aperiam expedita tempora?",
-        age: 23,
-        gender: 'M',
-        country: 'FR'
-    },
-    {
-        id: 2,
-        nickname: 'Celine',
-        avatar: 'https://i.pravatar.cc/150?u=celine',
-        bio: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis corrupti ipsum odio, sed aut ipsam provident blanditiis, molestias veritatis impedit aperiam expedita tempora?",
-        age: 34,
-        gender: 'M',
-        country: 'FR'
-    },
-    {
-        id: 3,
-        nickname: 'Natouche',
-        avatar: 'https://i.pravatar.cc/150?u=nath',
-        bio: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis corrupti ipsum odio, sed aut ipsam provident blanditiis, molestias veritatis impedit aperiam expedita tempora?",
-        age: 26,
-        gender: 'M',
-        country: 'BE'
-    }
+  {
+    id: 1,
+    nickname: 'Gevail',
+    avatar: 'https://i.pravatar.cc/150?u=gevail',
+    bio: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates tempore aliquam, et error quos nam veritatis cum eos nostrum laborum perferendis facilis eaque! Placeat alias dolore inventore itaque! Recusandae sapiente et suscipit labore, provident obcaecati ea. Doloribus alias qui sapiente dolorum nobis voluptas omnis quasi. Odio iusto repellat provident in.',
+    age: 23,
+    job: 'Développeur',
+    gender: 'M',
+    country: 'FR'
+  },
+  {
+    id: 2,
+    nickname: 'Celine',
+    avatar: 'https://i.pravatar.cc/150?u=celine',
+    bio: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis corrupti ipsum odio, sed aut ipsam provident blanditiis, molestias veritatis impedit aperiam expedita tempora?',
+    age: 34,
+    gender: 'M',
+    job: 'Graphiste',
+    country: 'FR'
+  },
+  {
+    id: 3,
+    nickname: 'Natouche',
+    avatar: 'https://i.pravatar.cc/150?u=nath',
+    bio: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis corrupti ipsum odio, sed aut ipsam provident blanditiis, molestias veritatis impedit aperiam expedita tempora?',
+    age: 26,
+    job: 'Commercial',
+    gender: 'M',
+    country: 'BE'
+  }
 ]
+
+const selected = ref(null)
 </script>
 
 <template>
-    <h1 class="titleView">A propos</h1>
-    <div class="card-grid">
-        <AboutCard v-for="user in team" :key="user.id" :user="user"/>
-    </div>
-
+  <h1 class="titleView">A propos</h1>
+  <div class="card-grid">
+    <AboutCard
+      v-for="user in team"
+      :key="user.id"
+      :user="user"
+      @opened="selected = user.id"
+      :open="selected === user.id"
+    />
+    <!-- Ici, "@opened = selected = user.id" user.id peut être = à $event.id -->
+  </div>
 </template>
 
 <style scoped lang="scss">
-
 .titleView {
-    text-align: center;
-    font-size: 3rem;
-    font-weight: bold;
-    color : #2c3e50;
-    margin: 1em;
+  text-align: center;
+  font-size: 3rem;
+  font-weight: bold;
+  color: #2c3e50;
+  margin: 1em;
 }
 
-.card-grid{
-    display: grid;
-    grid-template-columns: repeat(3, 1fr);
-    grid-template-rows: repeat(2, 1fr);
-    width: 100%;
-    height: 100%;
+.card-grid {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-template-rows: repeat(2, 1fr);
+  width: 100%;
+  height: 100%;
+  margin-top: 8rem;
 }
-
 </style>
