a442ed59ef3f55a834c8f7ccac8936fc9bd97f3c
Loic Delplace <loic.delplace59@gmail.com>
2023-08-22 12:40:45 +0200

    📄 Added Movie Card Component

---
+++ src/components/MovieCardComponent.vue
@@ -0,0 +1,18 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="movieCard">
+        <div class="movieCard-top">
+            <img src="" alt="">
+            <div class="movieNote">
+                <p>0</p>
+            </div>
+        </div>
+        <div class="movieCard-bottom">
+            <p class="movieTitle"></p>
+            <p class="moviesReleaseDate"></p>
+        </div>
+    </div>
+</template>
