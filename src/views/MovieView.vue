f5a64aa7f2161a6cdafcabea2518ce9e71eb367d
Loic Delplace <loic.delplace59@gmail.com>
2023-08-23 16:02:06 +0200

    ⚙️ Add loading

--- src/views/MovieView.vue
+++ src/views/MovieView.vue
@@ -2,17 +2,29 @@
 import { getMovies } from '../services/api'
 import { ref } from 'vue'
 import MovieCard from '@/components/cards/MovieCardComponent.vue'
+import Loader from '@/components/LoaderComponent.vue'
+// import Button from '@/components/buttons/ButtonMoreComponent.vue';
 
 const movies = ref([])
+const loading = ref(true)
 getMovies().then((response) => {
-  movies.value = response
+  setTimeout(() => {
+    movies.value = response
+    loading.value = false
+  }, 1000)
 })
 </script>
 
 <template>
-  <h1 class="titleView">Films</h1>
-  <div class="movieCard-container">
-    <MovieCard v-for="movie in movies" :key="movie.id" :movie="movie" />
+  <div class="container" v-if="!loading">
+    <h1 class="titleView">Films</h1>
+    <div class="movieCard-container">
+      <MovieCard v-for="movie in movies" :key="movie.id" :movie="movie" />
+    </div>
+  </div>
+
+  <div class="loaderContainer" v-else>
+    <Loader message="Nous récupérons les films !" />
   </div>
 </template>
 
@@ -25,5 +37,13 @@ getMovies().then((response) => {
   align-items: center;
   gap: 1rem;
   margin: 0 auto;
+}
+
+.loaderContainer {
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 </style>
