4a3b62ca513bb0a8a9356ab8cd8d7304eda0062d
Loic Delplace <loic.delplace59@gmail.com>
2023-08-23 16:02:10 +0200

    ⚙️ Fixing footer... again

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -58,9 +58,8 @@ const selected = ref(null)
 .card-grid {
   display: grid;
   grid-template-columns: repeat(3, 1fr);
-  grid-template-rows: repeat(2, 1fr);
   width: 100%;
   height: 100%;
-  margin-top: 8rem;
+  margin-top: 1rem;
 }
 </style>
