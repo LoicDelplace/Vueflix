8d5dbe6e0aa0a943f58dcae72385d1faf8ed2c7b
Loic Delplace <loic.delplace59@gmail.com>
2023-08-23 13:32:47 +0200

    ✨ + limited nb of movies

--- src/views/MovieView.vue
+++ src/views/MovieView.vue
@@ -1,3 +1,31 @@
+<script setup>
+import MovieCard from '@/components/cards/MovieCardComponent.vue'
+import { getMovies } from '../services/api'
+import { onBeforeMount, ref } from 'vue'
+
+const movies = ref([])
+onBeforeMount(() => {
+  getMovies(5).then((response) => {
+    movies.value = response
+  })
+})
+</script>
+
 <template>
-    <h1 class="titleView">Films</h1>
+  <h1 class="titleView">Films</h1>
+  <div class="movieCard-container">
+    <MovieCard v-for="movie in movies" :key="movie.id" :movie="movie" />
+  </div>
 </template>
+
+<style scoped lang="scss">
+.movieCard-container {
+  width: 80%;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+  gap: 1rem;
+  margin: 0 auto;
+}
+</style>
