ec629fdc80119137c8217a6222dbd9226b1fa305
Loic Delplace <loic.delplace59@gmail.com>
2023-08-24 17:09:35 +0200

    📄 Added Button Trailer Component

---
+++ src/components/buttons/ButtonTrailerComponent.vue
@@ -0,0 +1,26 @@
+<script setup>
+</script>
+
+<template>
+  <div class="button-trailer">
+    <div class="icon-container">
+      <img src="/play.svg">
+    </div>
+    <slot name="text"/>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.button-trailer {
+  display: flex;
+  cursor: pointer;
+  width: fit-content;
+  align-items: center;
+  justify-content: center;
+}
+
+.icon-container{
+  width: 48px;
+  height: 48px;
+}
+</style>
